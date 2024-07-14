593fbe5faa93e38a209449b33ec44ae0dc075714
Vitalis Ian <viterlis254@gmail.com>
2024-07-14 15:07:56 +0300

    mods

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-const db = require("./db.json");
+
 
 server.use(middlewares);
 server.use(router);
-
-server.listen(port);
+server.listen(3000, () => {
+  console.log('JSON Server is running on http://localhost:3000');
+});
